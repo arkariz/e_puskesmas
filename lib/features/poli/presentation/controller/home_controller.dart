45881db6b0cff148394cc3bf43b5b5bf70b69530
Muhammad Rizky <muhammadrisky1401@gmail.com>
2022-06-13 20:36:17 +0700

    add user cache

--- lib/features/poli/presentation/controller/home_controller.dart
+++ lib/features/poli/presentation/controller/home_controller.dart
@@ -1,15 +1,27 @@
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeController extends GetxController {
-  List<Map<String, dynamic>> pasien = Get.arguments;
-
   var namaPasien = "".obs;
+  var email = "".obs;
   var statusPasien = "".obs;
 
   @override
   void onInit() {
-    namaPasien(pasien.last['nama_lengkap']);
-    statusPasien(pasien.last['status_pasien']);
+    getPasien();
     super.onInit();
+  }
+
+  void getPasien() async {
+    final prefs = await SharedPreferences.getInstance();
+    final bool? isLogin = prefs.getBool("isLogin");
+
+    if (isLogin!) {
+      final List<String>? pasien = prefs.getStringList('pasien');
+
+      namaPasien(pasien![0]);
+      statusPasien(pasien[1]);
+      email(pasien[2]);
+    }
   }
 }
--- lib/features/userAuth/presentation/controller/login_controller.dart
+++ lib/features/userAuth/presentation/controller/login_controller.dart
@@ -2,6 +2,7 @@ import 'package:e_puskesmas/core/routes/app_pages.dart';
 import 'package:e_puskesmas/features/userAuth/data/datasources/pasien_sql.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginController extends GetxController {
   final emailController = TextEditingController().obs;
@@ -11,11 +12,25 @@ class LoginController extends GetxController {
   final sandiNode = FocusNode().obs;
 
   void loginPasien() async {
-    final pasien = await PasienSql.getSinglePasien(
-      emailController.value.text,
-      sandiController.value.text,
-    );
+    try {
+      final pasien = await PasienSql.getSinglePasien(
+        emailController.value.text,
+        sandiController.value.text,
+      );
 
-    Get.offNamed(Routes.HOME, arguments: pasien);
+      // Obtain shared preferences.
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setStringList('pasien', <String>[
+        pasien.last['nama_lengkap'],
+        pasien.last['status_pasien'],
+        pasien.last['email'],
+      ]);
+
+      await prefs.setBool('isLogin', true);
+
+      Get.offNamed(Routes.HOME);
+    } catch (e) {
+      Get.snackbar("E-Puskesmas", "Terjadi Kesalahan");
+    }
   }
 }
